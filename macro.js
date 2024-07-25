bcf6befbcf5aeb5fe36514594000389ef40cc0f7
Dondougo <kbthies@gmail.com>
2024-07-24 22:01:25 -0400

    Step 2
    Get it to work with the variables I need to pass into the macro

--- macro.js
+++ macro.js
@@ -1,3 +1,25 @@
-function runScript(userName) {
-  console.log(`Macro executed by: ${userName}`);
+/*
+The macro from the character sheet
+%{return await game.macros.getName('ISEKAI Roll').execute({chara: entity, statLabel:"Strength", rolledStat:"strmath", attack: true}) }%<br>${name}$ prepares to attack with Str
+%{consoleLog()}%
+
+What variables need to be passed?
+
+From the Game:
+game
+
+From the Character:
+chara
+attack
+statLabel
+rolledStat
+
+*/
+
+
+function runScript(game) {
+  console.log("game:");
+  console.log(game);
+  console.log(chara);
+  console.log("Is attack?: "attack + "<br>Stat:" + statLabel + "<br>Stat Label:" + rolledStat);
 }
