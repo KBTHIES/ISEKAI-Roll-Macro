131889493c717b20a6db5697a01a7c284760144c
Dondougo <kbthies@gmail.com>
2024-07-24 22:20:03 -0400

    Update macro.js

--- macro.js
+++ macro.js
@@ -18,8 +18,7 @@ rolledStat
 
 
 function runScript(game, chara, attack, statLabel, rolledStat) {
-  console.log("game:");
   console.log(game);
   console.log(chara);
-  console.log("Is attack?: "${attack}"<br>Stat:"${statLabel}"<br>Stat Label:"${rolledStat});
+  console.log('Is attack?: '${attack}'<br>Stat:'${statLabel}'<br>Stat Label:'${rolledStat};
 }
