295491fc48f1ae32f4b4c0caff56286eb950129c
Dondougo <kbthies@gmail.com>
2024-08-05 22:41:31 -0400

    Turn Check Successes into <detail>
    Now the Check Successes bar will have the dropdown menu to inspect the roll

--- macro.js
+++ macro.js
@@ -946,8 +946,9 @@ function runScript(game, chara, attack, statLabel, rolledStat) {
               chatMessage += ` check</h4>`;
               // Xd6 vs Threshold
               chatMessage += (`<h3><span title="` + thresholdTooltip + `">${diceNumber}d6 against a threshold of ${verifiedThreshold}` + multiplierMessage + `</span></h3>`);
-              chatMessage += (`<h2>${roll.total} Successes` + postedSuccessMessage + `</h2>`);
-              chatMessage += "</p><div style=\"padding:5px;line-height:2em;\"><span title=\"" + thresholdTooltip + "\">" + diceMessage + "</span></div><hr>";
+              chatMessage += (`"<details><summary style=\"font-size: 1.25em\">"${roll.total} Successes` + postedSuccessMessage + `</summary>`);
+              ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+              chatMessage += "<div style=\"padding:5px;line-height:2em;\"><span title=\"" + thresholdTooltip + "\">" + diceMessage + "</span></div><hr></details>";
             }
 
             // Modifiers
